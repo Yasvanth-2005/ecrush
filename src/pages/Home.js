73ed244af5dbe26c4d36b2630bdc89d9c319ad87
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2023-06-24 12:38:47 +0530

    Hero Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
 import Header from "../components/Header";
+import heroImg from "../images/hero-img.png";
 
 export default function Home() {
   return (
     <>
-        <Header />
+      <Header />
+      <section id="hero" className="container-fluid d-flex justify-content-center align-items-center">
+        <div class="hero-content container">
+            <div className="heading">
+              <h2 className="title">E-Crush</h2>
+              <h4 className="sub-title">Provision for your vision</h4>
+              <p className="context">
+                This site is intended to provide the best resources to the RGUKTians.
+              </p>
+            </div>
+            <div className="img">
+              <img src={heroImg} alt="" className="img-fluid"/>
+            </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
