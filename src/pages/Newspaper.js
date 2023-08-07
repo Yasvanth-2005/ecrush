38ffaa1d9e77b0a082502b377f600bae5ae1273b
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2023-08-07 11:10:46 +0530

    small news change

--- src/pages/Newspaper.js
+++ src/pages/Newspaper.js
@@ -60,12 +60,11 @@ export default function Newspaper() {
                   className="card mb-4"
                   style={{ height: "100%", position: "relative" }}
                 >
-                  <div className="card-header">
+                  <div className="card-header" style={{ height: "200px" }}>
                     <img
                       alt={article.title}
                       src={article.urlToImage}
                       className="img-fluid"
-                      style={{ height: "200px" }}
                     />
                   </div>
                   <div className="card-body">
